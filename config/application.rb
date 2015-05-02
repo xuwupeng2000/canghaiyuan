5968d0738ab566ed5700b987d0fc74dbdfcd9ef3
Jack Wu <xuwupeng2000@gmail.com>
2015-05-02 12:10:06 +1200

    Rm bower

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ end
 
 module Canghaiyuan
   class Application < Rails::Application
-    require "/home/pengwu/projects/canghaiyuan/config/initializers/bower_rails.rb"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
